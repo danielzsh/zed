66d0044435b9c633880e707da82c4b5271c727fa
Kirill Bulatov <kirill@zed.dev>
2024-03-03 23:56:29 +0200

    Simplify the clicked gutter logic

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -4835,6 +4835,7 @@ impl Editor {
         })
     }
 
+    // TODO kb add editor tests
     pub fn revert_selected_chunks(&mut self, _: &RevertSelectedChunks, cx: &mut ViewContext<Self>) {
         let mut revert_changes = Vec::new();
 
